5e83dc9da24e43ded0c0b5b505045d5a5476e8c9
Aleks Fers <aleksfers@icloud.com>
2021-11-10 22:20:19 +0500

    Styles and handle events practice

--- src/App.js
+++ src/App.js
@@ -1,18 +1,70 @@
-import './App.css';
+import styles from './App.module.scss';
 import { Component } from 'react';
 import Car from './components/Car/Car';
 
 class App extends Component {
+  state = {
+    cars: [
+      { name: 'Ford', year: 2016, src: 'https://loremflickr.com/320/240/car' },
+      { name: 'Mazda', year: 2020, src: 'https://loremflickr.com/320/240/car' },
+    ],
+    pageTitle: 'React components',
+    showCars: false,
+  };
+
+  changeNameHandler = (name, index) => {
+    const car = this.state.cars[index];
+    const cars = [...this.state.cars];
+
+    cars.splice(index, 1, { ...car, name });
+
+    this.setState({
+      cars,
+    });
+  };
+
+  toggleCarsHandler = () => {
+    this.setState({
+      showCars: !this.state.showCars,
+    });
+  };
+
+  deleteHandler = (index) => {
+    const cars = [...this.state.cars];
+
+    cars.splice(index, 1);
+
+    this.setState({
+      cars,
+    });
+  };
+
   render() {
-    const divStyle = {
-      textAlign: 'center',
-    };
+    const { cars, pageTitle, showCars } = this.state;
 
     return (
-      <div className="App" style={divStyle}>
-        <h1>Hello World!!!</h1>
-        <Car name={'Ford'} year={2020} />
-        <Car name={'Mazda'} year={2018} />
+      <div className={styles.App}>
+        <h1>{pageTitle}</h1>
+
+        <div style={{ marginBottom: '10px' }}>
+          <button onClick={this.toggleCarsHandler}>Toggle cars</button>
+        </div>
+        <div className={styles.Cars}>
+          {showCars
+            ? cars.map((car, index) => (
+                <Car
+                  name={car.name}
+                  year={car.year}
+                  src={car.src}
+                  key={index}
+                  onChangeName={(event) =>
+                    this.changeNameHandler(event.target.value, index)
+                  }
+                  onDelete={this.deleteHandler.bind(this, index)}
+                />
+              ))
+            : 'Click for show cars ⬆️'}
+        </div>
       </div>
     );
   }
--- src/components/Car/Car.js
+++ src/components/Car/Car.js
@@ -1,17 +1,60 @@
 import PropTypes from 'prop-types';
+import Radium from 'radium';
+import './Car.scss';
 
-const Car = ({ name, year }) => (
-  <div>
-    <h3>Car name: {name}</h3>
-    <p>
-      Year: <strong>{year}</strong>
-    </p>
-  </div>
-);
+const Car = ({ name, year, onChangeName, onDelete, src }) => {
+  const inputClasses = ['input'];
+
+  if (name) {
+    inputClasses.push('green');
+  } else {
+    inputClasses.push('red');
+  }
+
+  if (name.length > 4) {
+    inputClasses.push('bold');
+  }
+
+  const style = {
+    border: '1px solid #ccc',
+    boxShadow: '0 4px 5px 0 rgba(0, 0, 0, 0.14)',
+    ':hover': {
+      border: '1px solid #aaa',
+      boxShadow: '0 4px 15px 0 rgba(0, 0, 0, 0.25)',
+      cursor: 'pointer',
+    },
+  };
+
+  return (
+    <div className="Car" style={style}>
+      <img
+        src={src}
+        alt={name}
+        style={{ marginBottom: '10px' }}
+        width="320"
+        height="240"
+      />
+      <h3>Car name: {name}</h3>
+      <p>
+        Year: <strong>{year}</strong>
+      </p>
+      <input
+        type="text"
+        onChange={onChangeName}
+        value={name}
+        className={inputClasses.join(' ')}
+      />
+      <button onClick={onDelete}>Delete</button>
+    </div>
+  );
+};
 
 Car.propTypes = {
   name: PropTypes.string,
+  src: PropTypes.string,
   year: PropTypes.number,
+  onChangeName: PropTypes.func,
+  onDelete: PropTypes.func,
 };
 
-export default Car;
+export default Radium(Car);
